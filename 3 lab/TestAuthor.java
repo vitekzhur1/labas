0597cd54af6e077ba4631f62480c497ac88d963c
vitekzhur1 <55838094+vitekzhur1@users.noreply.github.com>
2019-12-05 14:08:43 +0300

    Add files via upload

---
+++ 3 lab/TestAuthor.java
@@ -0,0 +1,11 @@
+package fifth;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Виктор", "vitek.zhur1@gmail.com",'M');
+        System.out.println(a1);
+        a1.setEmail("vitek.zhur1@gmail.com");
+        System.out.println(a1);
+    }
+}
+
